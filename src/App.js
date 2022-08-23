c071f8d2b3318e31a34a63a2134f7981d2c5e18f
홍여진 <69565849+hyj01622@users.noreply.github.com>
2022-08-24 00:36:55 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,156 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from 'styled-components';
+import { useTable, useBlockLayout, useResizeColumns } from 'react-table';
+
+import makeData from './mockData';
+
+const Styles = styled.div`
+  padding: 1rem;
+
+  .resizer {
+    background: none !important;
+  }
+
+  .isResizing {
+    background: none !important;
+  }
+
+  .table {
+    display: inline-block;
+    border-spacing: 0;
+    border: 1px solid black;
+
+    .tr {
+      :last-child {
+        .td {
+          border-bottom: 0;
+        }
+      }
+    }
+
+    .th,
+    .td {
+      margin: 0;
+      padding: 0.5rem;
+      border-bottom: 1px solid black;
+      border-right: 1px solid black;
+
+      ${
+        '' /* In this example we use an absolutely position resizer,
+       so this is required. */
+      }
+      position: relative;
+
+      :last-child {
+        border-right: 0;
+      }
+
+      .resizer {
+        display: inline-block;
+        background: blue;
+        width: 10px;
+        height: 100%;
+        position: absolute;
+        right: 0;
+        top: 0;
+        transform: translateX(50%);
+        z-index: 1;
+        ${'' /* prevents from scrolling while dragging on touch devices */}
+        touch-action:none;
+
+        &.isResizing {
+          background: red;
+        }
+      }
+    }
+  }
+`;
+
+function Table({ columns, data }) {
+  const defaultColumn = React.useMemo(
+    () => ({
+      minWidth: 80,
+      width: 150,
+      maxWidth: 200,
+    }),
+    []
+  );
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow, state, resetResizing } = useTable(
+    {
+      columns,
+      data,
+      defaultColumn,
+    },
+    useBlockLayout,
+    useResizeColumns
+  );
+
+  return (
+    <>
+      <button onClick={resetResizing}>Reset Resizing</button>
+      <div>
+        <div {...getTableProps()} className='table'>
+          <div>
+            {headerGroups.map((headerGroup) => (
+              <div {...headerGroup.getHeaderGroupProps()} className='tr'>
+                {headerGroup.headers.map((column) => (
+                  <div {...column.getHeaderProps()} className='th'>
+                    {column.render('Header')}
+                    {/* Use column.getResizerProps to hook up the events correctly */}
+                    <div {...column.getResizerProps()} className={`resizer ${column.isResizing ? 'isResizing' : ''}`} />
+                  </div>
+                ))}
+              </div>
+            ))}
+          </div>
+
+          <div {...getTableBodyProps()}>
+            {rows.map((row, i) => {
+              prepareRow(row);
+              return (
+                <div {...row.getRowProps()} className='tr'>
+                  {row.cells.map((cell) => {
+                    return (
+                      <div {...cell.getCellProps()} className='td'>
+                        {cell.render('Cell')}
+                      </div>
+                    );
+                  })}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+      <pre>
+        <code>{JSON.stringify(state, null, 2)}</code>
+      </pre>
+    </>
+  );
+}
 
 function App() {
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'tablegood',
+        accessor: 'tablegood',
+      },
+      {
+        Header: 'tablecool',
+        accessor: 'tablecool',
+      },
+    ],
+    []
+  );
+
+  const data = React.useMemo(() => [{ tablegood: 'ho', tablecool: 'hi' }], []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Styles>
+      <Table columns={columns} data={data} />
+    </Styles>
   );
 }
 
